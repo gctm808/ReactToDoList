35ab1ca9269a1b2522fd52c130a72d41eaa4854b
Garrett Moore <mooreg2@seattleu.edu>
2019-02-12 13:16:15 -0800

    Homepage enhancements

--- router2/src/components/home.js
+++ router2/src/components/home.js
@@ -6,9 +6,13 @@ const Home = () => {
     return(
         <div className='home'>
             <h1>GOLD SHADES</h1>
-            <h2>NEW ALBUM "GOLD SHADES" OUT NOW!</h2>
-            <button>
-                <NavLink exact to='/stream'>Stream Us</NavLink>
+            
+            <img src={require('../images/logo.png')} alt='logo' 
+            height='200px' width='350px' />
+            <h2>NEW ALBUM OUT NOW!</h2>
+            <button className='listenButton'>
+                <NavLink exact to='/stream' className='listenButton_link'>
+                Listen / Purchase</NavLink>
             </button>
         </div>
     )
