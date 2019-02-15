9118ee66eff06e65055fde92c611ebea4ac9307e
Garrett Moore <GMoore@nwadmin.com>
2019-02-14 16:33:09 -0800

    Adding css

--- router2/src/components/social.js
+++ router2/src/components/social.js
@@ -10,10 +10,10 @@ const Social = () => {
             <img src={require('../images/igPlaceholder.png')} alt='placeholder' height='300px' width='300px'/>
             <Twitter />
             <div className='socialLinks'>
-                <img src={require('../images/facebook.png')} alt='fbLogo' /> 
-                <img src={require('../images/soundcloud.png')} alt='soundcloudLogo' /> 
-                <img src={require('../images/spotify.png')} alt='spotifyLogo' /> 
-                <img src={require('../images/apple.png')} alt='appleLogo' /> 
+                <a href='facebook.com/goldshadesmusic' target='_blank'><img src={require('../images/facebook.png')} alt='fbLogo' /></a>
+                <a href='soundcloud.com' target='_blank'><img src={require('../images/soundcloud.png')} alt='soundcloudLogo' /></a> 
+                <a href='spotify.com' target='_blank'><img src={require('../images/spotify.png')} alt='spotifyLogo' /></a>
+                <a href='apple.com/music' target='_blank'><img src={require('../images/apple.png')} alt='appleLogo' /></a>
             </div>
         </div>
     )
